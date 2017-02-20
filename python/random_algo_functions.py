b846ff9018fa2d40da1075ba86855fe7d69e6cdc
Cohen Karnell <cohen.karnell@gmail.com>
2017-02-20 18:58:20 -0500

    Add a function I was asked to make in a job interview

--- python/random_algo_functions.py
+++ python/random_algo_functions.py
@@ -1,3 +1,7 @@
+# For a list of integers that also contains nested
+# lists of integers, return a flattened list.
+# e.g. [1, [2, 3], [4, [5, 6], 7], 8, [9, [10]]]
+# would return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 def flattener(l):
     flattened = []
     def flattener_recurse(sub_list):
@@ -10,7 +14,18 @@ def flattener(l):
     flattener_recurse(l)
     return flattened
 
+# func("Hello ")("world!") returns "Hello world!".
+# This is a functional question asked for a job interview.
+def func(str1):
+    final_str = str1
+
+    def return_func(str2):
+        return final_str + str2
+    
+    return return_func
 
 if __name__ == "__main__":
     array = [1, 2, [3, 4, [5, 6], 7, 8], [9, 10]]
     print flattener(array)
+
+    print func("Hello ")("world!")
